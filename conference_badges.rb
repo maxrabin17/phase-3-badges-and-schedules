83f39a60dc29fa7f544f34f3e3abb047e6b1d186
maxrabin <max.rabin17@gmail.com>
2021-06-25 12:07:51 -0400

    commit tree 1c96c5a7ec9e29b1084177d26c0877787399b7ed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,26 +1,26 @@
 # Write your code here.
 def badge_maker(name)
-    "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
-    array.map do |a|
+  array.map do |a|
     "Hello, my name is #{a}."
-    end
+  end
 end
 
 def assign_rooms(speakers)
-    speakers.map.with_index(1) do |speaker, r|
-        "Hello, #{speaker}! You'll be assigned to room #{r}!"
-    end
+  speakers.map.with_index(1) do |speaker, r|
+    "Hello, #{speaker}! You'll be assigned to room #{r}!"
+  end
 end
 
 def printer(people)
-    batch_badge_creator(people).each do |badge|
-        puts badge
-    end
+  batch_badge_creator(people).each do |badge|
+    puts badge
+  end
 
-    assign_rooms(people).each do |assign|
-        puts assign
-    end
+  assign_rooms(people).each do |assign|
+    puts assign
+  end
 end
